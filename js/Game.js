dc7f5166ba078b6155b166713062fe236a925db7
Nick <komarnickolas@gmail.com>
2017-06-16 15:13:53 -0600

    Working mazing

--- js/Game.js
+++ js/Game.js
@@ -7,10 +7,12 @@ class Game{
         this.ctx = this.canvas.getContext("2d");
         this.towers = [];
         this.walls = [];
+        this.mobs = [];
+        this.path = [];
         this.maze = [
             ['w','s','w','w','T','w','w','e','w'],
             ['w',' ','w',' ',' ',' ','T',' ','w'],
-            ['T',' ','w',' ','w',' ','w',' ','T'],
+            ['T',' ','w',' ','T',' ','w',' ','T'],
             ['w',' ','T',' ','w',' ','w',' ','w'],
             ['w',' ','w',' ','T',' ','T',' ','w'],
             ['w',' ',' ',' ','w',' ',' ',' ','T'],
@@ -20,19 +22,39 @@ class Game{
             for(let j = 0, x=0; j<this.maze[0].length; j++, x+=120){
                 if(this.maze[i][j] === 'T') {
                     this.towers.push(new Tower("img/Tower.png", x, y, 118, 118, 40));
+                    this.walls.push(new Wall("img/wall.jpg", x, y, 118, 118, 40));
                 }else if(this.maze[i][j] === 'w'){
                     this.walls.push(new Wall("img/wall.jpg", x, y, 118, 118, 40));
                 }
             }
         }
+        for(let j = 0; j<this.maze[0].length; j++){
+            for(let i = 0; i<this.maze.length; i++){
+                if(this.maze[i][j] === 's' || this.maze[i][j] === ' ' || this.maze[i][j] === 'e'){
+                    this.path.push([i,j]);
+                }
+            }
+        }
+        for(let i = 0; i<120; i+=120){
+            let mob = new Mob("img/mob_sprites.png", 120,i, 80,56,18, [
+                [25,30],
+                [0,5],
+                [13,17],
+                [36, 40]
+            ],this.path);
+            this.mobs.push(mob);
+        }
     }
     animate(){
         this.canvas.width = this.canvas.width;
+        for(let i = 0; i<this.walls.length; i++) {
+            this.walls[i].animate(this.ctx);
+        }
         for(let i = 0; i<this.towers.length; i++) {
             this.towers[i].animate(this.ctx);
         }
-        for(let i = 0; i<this.walls.length; i++) {
-            this.walls[i].animate(this.ctx);
+        for(let i = 0; i<this.mobs.length; i++) {
+            this.mobs[i].animate(this.ctx);
         }
     }
 }
@@ -45,6 +67,7 @@ class SpriteSheet{
         this.frameHeight = frameHeight;
         this.frameSpeed = frameSpeed;
         this.scenes = scenes;
+        this.current_scene = 0;
         let self = this;
         this.image.onload = function() {
             self.frames = Math.floor(self.image.width / self.frameWidth);
@@ -56,7 +79,7 @@ class SpriteSheet{
     }
     update(){
         this.sequence = [];
-        for(let f = this.scenes[0][0]; f<=this.scenes[0][1]; f++){
+        for(let f = this.scenes[this.current_scene][0]; f<=this.scenes[this.current_scene][1]; f++){
             this.sequence.push(f);
         }
         if(this.counter == (this.frameSpeed - 1)){
@@ -98,3 +121,65 @@ class Wall extends Sprite{
         super(src,x,y, frameWidth,frameHeight, frameSpeed, [[0,0]]);
     }
 }
+class Mob extends Sprite{
+    constructor(src, x,y, frameWidth,frameHeight, frameSpeed, scenes, path){
+        super(src,x,y, frameWidth,frameHeight, frameSpeed, scenes);
+        this.path = path;
+        this.current = 0;
+    }
+    down(){
+        this.spritesheet.current_scene = 0;
+        if(this.spritesheet.y + 1 < 650) {
+            this.spritesheet.y++;
+        }
+    }
+    left(){
+        this.spritesheet.current_scene = 2;
+        if(this.spritesheet.x - 1 > 0) {
+            this.spritesheet.x--;
+        }
+    }
+    right(){
+        this.spritesheet.current_scene = 1;
+        if(this.spritesheet.x + 1 <1800) {
+            this.spritesheet.x++;
+        }
+    }
+    up(){
+        this.spritesheet.current_scene = 3;
+        if(this.spritesheet.y - 1 > 0) {
+            this.spritesheet.y--;
+        }
+    }
+    move(){
+    }
+    animate(ctx){
+        this.move();
+        this.spritesheet.update();
+        this.spritesheet.animate(ctx);
+    }
+}
+var Key_Codes = {
+    32: 'space',
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    87: 'w',
+    65: 'a',
+    83: 's',
+    68: 'd'
+};
+var Key_Status = [];
+document.onkeydown = function (e) {
+    if(Key_Codes[e.keyCode]){
+        e.preventDefault();
+        Key_Status[Key_Codes[e.keyCode]] = true
+    }
+};
+document.onkeyup = function (e) {
+    if(Key_Codes[e.keyCode]){
+        e.preventDefault();
+        Key_Status[Key_Codes[e.keyCode]] = false;
+    }
+};
---
+++ js/Pathing.js
@@ -0,0 +1,18 @@
+/**
+ * Created by nick on 6/16/17.
+ */
+class Node{
+    constructor(){
+
+    }
+}
+class Graph{
+    constructor(input){
+
+    }
+}
+class Pathing{
+    constructor(){
+
+    }
+}
