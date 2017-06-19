7ecfd83a990c97353c51d590b7c49f1013c2579d
Nick <komarnickolas@gmail.com>
2017-06-19 13:15:56 -0600

    Updated functionality. Game is working, can select and place towers in proper position. Economy is next

--- js/Game.js
+++ js/Game.js
@@ -7,8 +7,10 @@ class Game {
         this.map = new Map();
         this.towers = [];
         this.waves = [];
-        this.tower_slots = [];
-        this.tower_selectors = [];
+        this.tower_slots = {
+            slots: [],
+            active_slot: null
+        };
         this.current_wave = -1;
         this.idle = true;
         this.lives = 100;
@@ -17,16 +19,18 @@ class Game {
             for (let j = 0, x = 0; j < this.map.maze.tower_locations[i].length; j++, x++) {
                 switch (this.map.maze.tower_locations[i][j]) {
                     case 'T':
-                        this.tower_slots.push(new Tower_Slot(types.ux.empty, x, y));
-                        this.tower_selectors.push(new Tower_Selector(x, y));
+                        this.tower_slots.slots.push(new Tower_Slot(types.ux.empty, x, y));
                         break;
                 }
             }
         }
-        this.waves.push(new Wave(15,types.mobs.super_soldier,this.map.maze.path));
-        this.waves.push(new Wave(15,types.mobs.cyborg,this.map.maze.path));
-        this.waves.push(new Wave(15,types.mobs.robot,this.map.maze.path));
-        this.waves.push(new Wave(15,types.mobs.robot,this.map.maze.path));
+        this.waves.push(new Wave(5, types.mobs.soldier, this.map.maze.path));
+        this.waves.push(new Wave(10, types.mobs.soldier, this.map.maze.path));
+        this.waves.push(new Wave(15, types.mobs.soldier, this.map.maze.path));
+        this.waves.push(new Wave(5, types.mobs.robot, this.map.maze.path));
+        this.waves.push(new Wave(10, types.mobs.robot, this.map.maze.path));
+        this.waves.push(new Wave(10, types.mobs.super_soldier, this.map.maze.path));
+        this.waves.push(new Wave(5, types.mobs.cyborg, this.map.maze.path));
         console.log(this.waves);
         $('#start').click(function () {
              self.idle = false;
@@ -58,15 +62,33 @@ class Game {
                 });
             }
         } else {
+            for (let i = 0; i < this.tower_slots.slots.length; i++) {
+                this.tower_slots.slots[i].draw();
+            }
             this.towers.forEach(function (tower) {
                 tower.draw();
             });
-            for(let i = 0; i<this.tower_slots.length; i++){
-                this.tower_slots[i].draw();
-                if(this.tower_slots[i].clicked()) {
-                    if(this.tower_selectors.selected === undefined) {
-                        this.tower_selectors[i].draw();
-                    }
+            if (this.tower_slots.active_slot !== null) {
+                if (this.tower_slots.active_slot.towers[0].clicked()) {
+                    let t = this.tower_slots.active_slot.towers[0];
+                    t.x -= 64;
+                    this.towers.push(t);
+                    this.tower_slots.slots.splice(this.tower_slots.slots.indexOf(this.tower_slots.active_slot), 1);
+                    this.tower_slots.active_slot = null;
+                } else if (this.tower_slots.active_slot.towers[1].clicked()) {
+                    let t = this.tower_slots.active_slot.towers[1];
+                    t.x -= 64;
+                    t.y -= 64;
+                    this.towers.push(t);
+                    this.tower_slots.slots.splice(this.tower_slots.slots.indexOf(this.tower_slots.active_slot), 1);
+                    this.tower_slots.active_slot = null;
+                } else {
+                    this.tower_slots.active_slot.drawTowers();
+                }
+            }
+            for (let i = 0; i < this.tower_slots.slots.length; i++) {
+                if (this.tower_slots.slots[i].clicked()) {
+                    this.tower_slots.active_slot = this.tower_slots.slots[i];
                 }
             }
         }
@@ -75,23 +97,18 @@ class Game {
 class Tower_Slot extends Sprite{
     constructor(type, x, y) {
         super(x * 64, y * 64, 64, 64, type.x * 64, type.y * 64, 64, 64);
-    }
-    draw() {
-        ctx.drawImage(this.image, this.sx, this.sy, this.swidth, this.sheight, this.x, this.y, this.width, this.height);
-    }
-}
-class Tower_Selector{
-    constructor(x,y){
         this.rx = x;
         this.ry = y;
         this.width = 64;
         this.height = 64;
         this.towers = [
-            new Tower(types.towers.cannon.level_1, this.rx+1, this.ry),
-            new Tower(types.towers.missile.level_1, this.rx+1,this.ry+1),
+            new Tower(types.towers.cannon.level_1, this.rx + 1, this.ry),
+            new Tower(types.towers.missile.level_1, this.rx + 1, this.ry + 1),
         ];
     }
-    draw(){
+
+    drawTowers() {
+        ctx.fillRect(this.towers[0].x + 64, this.towers[0], this.towers[0].width, this.towers[0].height + this.towers[1].height);
         this.towers[0].draw();
         ctx.fillText(this.towers[0].type, this.towers[0].x + 64, this.towers[0].y + 20);
         ctx.fillText("Level: " + this.towers[0].level, this.towers[0].x + 64, this.towers[0].y + 30);
@@ -100,12 +117,9 @@ class Tower_Selector{
         ctx.fillText(this.towers[1].type, this.towers[1].x + 64, this.towers[1].y + 20);
         ctx.fillText("Level: " + this.towers[1].level, this.towers[1].x + 64, this.towers[1].y + 30);
         ctx.fillText("Atk: " + this.towers[1].attack, this.towers[1].x + 64, this.towers[1].y + 40);
-        if(this.towers[0].clicked()){
-            this.selected = this.towers[0];
-        }
-        if(this.towers[1].clicked()){
-            this.selected = this.towers[1];
-        }
+    }
+    draw() {
+        ctx.drawImage(this.image, this.sx, this.sy, this.swidth, this.sheight, this.x, this.y, this.width, this.height);
     }
 }
 class Wave{
