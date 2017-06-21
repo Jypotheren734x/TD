ad68c10243e06027c39d1c05edf71e01c285ceec
Nick <komarnickolas@gmail.com>
2017-06-21 17:53:26 -0600

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /**
  * Created by nick on 6/16/17.
  */
+require('helpers');
+require('Pathing');
+require('Map');
+require('Sprites');
+require('Game');
 $(document).ready(function () {
     let game = new Game();
 
