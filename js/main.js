47668eca894be2eed9fffafe20f2506b20961316
Nickolas Komarnitsky <komarnickolas@gmail.com>
2017-06-21 22:42:14 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 /**
  * Created by nick on 6/16/17.
  */
-require('helpers');
-require('Pathing');
-require('Map');
-require('Sprites');
-require('Game');
 $(document).ready(function () {
     let game = new Game();
 
