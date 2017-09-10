710ef66f3bd3afe11d16dab51395ea97b7d9d9d8
Nick <komarnickolas@gmail.com>
2017-09-09 20:12:26 -0600

    Updates

--- js/Game.js
+++ js/Game.js
@@ -83,8 +83,8 @@ class Game {
         ctx.fillText("Kills: ", this.map.maze.background[0].length * scale, 138);
         ctx.fillText("Money: ", this.map.maze.background[0].length * scale, 202);
         ctx.fillText("Instructions:", this.map.maze.background[0].length * scale, 396);
-        ctx.fillText("Slowly drag and drop towers", this.map.maze.background[0].length * scale, 420);
-        ctx.fillText("Hit the start button", this.map.maze.background[0].length * scale, 440);
+        ctx.fillText("1. Slowly drag and drop towers.", this.map.maze.background[0].length * scale, 420);
+        ctx.fillText("2. Hit the start button.", this.map.maze.background[0].length * scale, 440);
     }
     run() {
         let self = this;
